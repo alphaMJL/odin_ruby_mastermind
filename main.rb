58ccfad88325b79a920283eec497db71b1cf29c6
alphaMJL <alphaMJL@github>
2023-09-27 16:23:25 -0400

    added move input, move checking

--- main.rb
+++ main.rb
@@ -9,6 +9,7 @@ class Board
 
   def reset_board
     @moves = []
+  end
 
   def draw_board(moves)
     board = <<-BOARD
@@ -33,7 +34,7 @@ class Board
       code_1, code_2, code_3, code_4, correct_colors, correct_positions = move
   
       move_row = <<-MOVE
-    | #{code_1} | #{code_2} | #{code_3} | #{code_4} |  | #{correct_colors} | #{correct_positions} |
+    | #{code_1.upcase} | #{code_2.upcase} | #{code_3.upcase} | #{code_4.upcase} |  | #{correct_colors} | #{correct_positions} |
     +---+---+---+---+  +---+---+
     MOVE
   
@@ -83,9 +84,9 @@ class Game
   end
 
 end
-#check if move is correct, build array to send to board by pushing status to array and update board, update Game.win
-class Game_logic
 
+class Game_logic
+#check if move is correct, build array to send to board by pushing status to array and update board, update Game.win
     def initialize
       @current_solution = []
       @current_move = []
@@ -110,14 +111,30 @@ class Game_logic
 
   def how_many_correct_colors(current_move, current_solution)
     total_correct = 0
-    #checking logic
-    @current_move.push(total_correct)
+  
+    # Iterate through each element of the current_move array
+    current_move.each_with_index do |color, index|
+      if current_solution.include?(color)
+        total_correct += 1
+        # To avoid counting the same color twice, remove it from current_solution
+        current_solution.delete_at(current_solution.index(color))
+      end
+    end
+  
+    total_correct
   end
-
+  
   def how_many_correct_positions(current_move, current_solution)
     total_correct = 0
-    #checking logic
-    @current_move.push(total_correct)
+  
+    # Iterate through each element of the current_move and current_solution arrays
+    current_move.each_with_index do |color, index|
+      if color == current_solution[index]
+        total_correct += 1
+      end
+    end
+  
+    total_correct
   end
 
   def update_board_moves
@@ -135,11 +152,21 @@ end
 module Input 
 
   def get_current_move
-    puts "Input current move. RBBR = Red, Blue, Blue, Red"
-    current_move = gets.chomp.downcase 
-    #error check move r b g y o p c m, 4 digits
-    return current_move
+    valid_colors = %w[r b g y o p c m]
+    
+    loop do
+      puts "Input current move. RBBR = Red, Blue, Blue, Red"
+      current_move = gets.chomp.downcase
+  
+      # Check if the input consists of valid colors and has exactly 4 characters
+      if current_move.match?(/^[#{valid_colors.join}]{4}$/)
+        return current_move
+      else
+        puts "Invalid input. Please enter a valid move with 4 characters from: #{valid_colors.join(', ')}"
+      end
+    end
   end
+  
 
 end
 
