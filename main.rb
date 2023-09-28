0102453fd9a97c53e7ce05854cd58b3f29871810
alphaMJL <alphaMJL@github>
2023-09-28 17:59:55 -0400

    improved formatting and commenting

--- main.rb
+++ main.rb
@@ -1,13 +1,12 @@
+# Input handling error checking
 module Input 
-
   def get_current_move
     valid_colors = %w[r b g y o p c m]
-    
     loop do
       puts ''
       puts "Input current move. RBBR = Red, Blue, Blue, Red"
       current_move = gets.chomp.downcase
-  
+
       # Check if the input consists of valid colors and has exactly 4 characters
       if current_move.match?(/^[#{valid_colors.join}]{4}$/)
         #p current_move.chars
@@ -20,12 +19,10 @@ module Input
 
   def enter_code
     valid_colors = %w[r b g y o p c m]
-    
     loop do
       puts ''
       puts "Enter a code for the computer to try to solve."
       current_move = gets.chomp.downcase
-  
       # Check if the input consists of valid colors and has exactly 4 characters
       if current_move.match?(/^[#{valid_colors.join}]{4}$/)
         #p current_move.chars
@@ -38,9 +35,7 @@ module Input
 
   def select_one_or_two
     loop do
-      
       player_selection = gets.chomp
-  
       # Check if 1 or 2
       if player_selection.match?(/^[12]$/)
 
@@ -49,13 +44,13 @@ module Input
         puts "Invalid input. Enter 1 or 2"
       end
     end
-  end  
-
+  end
 end
 
+# draws playspace and contains state of game board
 class Board
   attr_accessor :moves
-  
+
   def initialize
     @moves = []
   end
@@ -72,44 +67,42 @@ class Board
     | 1 | 2 | 3 | 4 |  | C | P |
     +---+---+---+---+  +---+---+
     BOARD
-  
+
     empty_row = <<-EMPTY
     |   |   |   |   |  |   |   |
     +---+---+---+---+  +---+---+
     EMPTY
-  
+
     # Add empty rows for unused moves
     empty_rows = empty_row * (12 - @moves.length)
     board += empty_rows
-  
+
     # Add moves starting from the bottom and moving upwards
     @moves.reverse_each do |move|
       code_1, code_2, code_3, code_4, correct_colors, correct_positions = move
-  
+
       move_row = <<-MOVE
     | #{code_1.upcase} | #{code_2.upcase} | #{code_3.upcase} | #{code_4.upcase} |  | #{correct_colors} | #{correct_positions} |
     +---+---+---+---+  +---+---+
     MOVE
-  
+
       board += move_row
     end
-      
+
     # Legend for colors and status
     legend = <<-LEGEND
-  
+
     Color Legend:
     R - Red    | B - Blue   | G - Green | Y - Yellow
     O - Orange | P - Purple | C - Cyan  | M - Magenta
-  
+
     Status Legend:
     C - Correct Color   | P - Correct Position
     LEGEND
-  
+
     puts board
     puts legend
   end
-
-  
 
   def draw_opening
     opening_screen = <<-OPENING
@@ -157,6 +150,7 @@ class Board
   end
 end
 
+# Game loops, game loop state variables
 class Game
   include Input
   attr_accessor :end, :win
@@ -166,56 +160,49 @@ class Game
     @end = false
   end
 
-  
   def reset_game
     @end = false
+    @win = false
   end
-  
 
   def play(board, logic)
     loop do
-      system "clear"
+      system 'clear'
       board.draw_opening
-    
-    
       case select_one_or_two
       when '1'
         play_as_player(board, logic)
       when '2'
         play_as_cpu(board, logic)
       else
-        puts "INPUT ERROR EXITING" #TESTING
+        puts 'INPUT ERROR EXITING' # TESTING
       end
-      
-
       puts ''
       puts 'Enter 1 to play again'
       puts 'Enter 2 to exit'
 
       if select_one_or_two == '2'
         exit
-      else 
+      else
         reset_game
         board.reset_board
         logic.reset_game
       end
-      
     end
   end
 
   def play_as_player(board, logic)
-    system "clear"
-    puts ['','','']
+    system 'clear'
+    puts ['', '', '']
     board.draw_board
-    
     until @end
       # get error checked move, pass to logic: win? how many color right? how many position right? pass array to board)
       board.moves.push(logic.do_turn(get_current_move))
-      system "clear"
+      system 'clear'
       puts ''
-      p logic.current_solution #TESTING - replace with puts '' for proper gamespace draw
+      p logic.current_solution # TESTING - replace with puts '' for proper gamespace draw
       puts ''
-      
+
       board.draw_board
       if board.moves.length >= 12
         puts ''
@@ -227,23 +214,19 @@ class Game
 
       end
     end
-
-    
-
   end
 
   def play_as_cpu(board, logic)
-    system "clear"
-    puts ['','','']
+    system 'clear'
+    puts ['' ,'', '']
     board.draw_board
     logic.current_solution = enter_code
     until @end
       # get error checked move, pass to logic: win? how many color right? how many position right? pass array to board)
-      
       board.moves.push(logic.do_turn(logic.cpu_move)) 
-      system "clear"
+      system 'clear'
       puts "You selected the following code. The computer can't see it. I swear..."
-      p logic.current_solution 
+      p logic.current_solution
       puts ''
       board.draw_board
       sleep(rand(2..3))
@@ -257,19 +240,15 @@ class Game
       end
     end
   end
-
 end
 
-
-
-
+# move checking, current game state
 class Game_logic
-
   attr_accessor :current_solution
 
   def initialize(game)
     @current_solution = []
-    #@current_move = []
+    @current_move = []
     @game = game
     create_solution
   end
@@ -279,10 +258,10 @@ class Game_logic
     create_solution
     @current_move = []
   end
-  
+
   def create_solution
     possible_moves = %w[r b g y o p c m]
-    #create random solution
+    # create random solution
     4.times {@current_solution.push(possible_moves[rand(0..7)])}
   end
 
@@ -299,17 +278,15 @@ class Game_logic
     # create hash from solution to check against
     count_hash = Hash.new(0)
     current_solution.each { |element| count_hash[element] += 1 }
-    #take current_move and look at each element? if element exists in hash and has a value > 1 increase total_correct. minus 1 from hash counter(prevent duplicates)
+    # take current_move and look at each element? if element exists in hash and has a value > 1 increase total_correct. minus 1 from hash counter(prevent duplicates)
     current_move.each do |element2|
-
-        if count_hash.key?(element2) && count_hash[element2] > 0
-          
-          total_correct += 1
-          count_hash[element2] -= 1
-        end
+      if count_hash.key?(element2) && count_hash[element2] > 0
+        total_correct += 1
+        count_hash[element2] -= 1
       end
-      # push to the array which will go to board for drawing to console
-      @current_move.push(total_correct)
+    end
+    # push to the array which will go to board for drawing to console
+    @current_move.push(total_correct)
   end
 
   def how_many_correct_positions(current_move, current_solution)
@@ -323,32 +300,31 @@ class Game_logic
     end
     # push to the array which will go to board for drawing to console
     @current_move.push(total_correct)
-    
   end
 
   def update_board_moves
     @board.moves = board.moves.push(@current_move)
   end
-  #check if move is correct, build array to return to game loop
+
+  # check if move is correct, build array to return to game loop
   def do_turn(current_move)
     is_current_move_win(current_move, @current_solution)
     how_many_correct_colors(@current_move, @current_solution)
     how_many_correct_positions(@current_move, @current_solution)
-    return @current_move
-    #update_board_moves
+    @current_move
+    # update_board_moves
   end
+
   def cpu_move
-    #return array of 4 strings of valid moves
+    # return array of 4 strings of valid moves
     valid_colors = %w[r b g y o p c m]
     move = []
 
     4.times do
-       #Randomly select a valid color from the valid_colors array
+       # Randomly select a valid color from the valid_colors array
       move << valid_colors.sample
     end
-
-    return move
-    
+    move
   end
 end
 
